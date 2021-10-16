6289007781f8eaabe34004a46ac27814fecaa066
Chipzstar <chipzstar.dev@googlemail.com>
2021-10-16 14:16:15 +0100

    update signup page UI

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,6 @@
 import './Signup.css';
-import logo from '../../img/secondslogin.svg';
+import logo from '../../img/seconds-logo-black.svg';
+import eye from '../../img/eye.svg';
 import { Formik } from 'formik';
 import { Link } from 'react-router-dom';
 import { authUser } from '../../store/actions/auth';
@@ -19,169 +20,222 @@ export default function Signup(props) {
 	}, [props.location]);
 
 	return (
-		<div className='signupPage container-fluid pt-3 pb-5'>
-			<div className='d-flex pt-5'>
-				<img className='mx-auto img-fluid' src={logo} alt='' />
-			</div>
-			<div className='signupContainer w-sm mt-5 mx-auto py-5 px-md-5 px-sm-3'>
-				<div className='d-flex flex-grow-1 flex-column justify-content-center'>
-					<h4 className='getStarted pb-4'>Get Started</h4>
-					{errors.message && (
-						<div className='alert alert-danger alert-dismissible' role='alert'>
-							<span>{errors.message}</span>
-							<button onClick={() => dispatch(removeError())} type='button' className='btn btn-close' />
+		<div className='signupPage bg-white container-fluid'>
+			<div className='row h-100'>
+				<div className='col-sm-3 d-none d-md-block h-100 p-4 signup-aside'>
+					<div className='d-flex flex-column h-100'>
+						<img src={logo} alt='' className='img-fluid' width={200} />
+						<div className='d-flex flex-grow-1   flex-column justify-content-center'>
+							<div className='mt-4'>
+								<h1 className="signup-aside-header">Strong coverage across the UK</h1>
+								<p className='text-wrap signup-aside-text'>
+									With over 7 fleet providers, your business can provide the best delivery experiences
+									to your customers.
+								</p>
+							</div>
+							<div className='mt-4'>
+								<h1 className="signup-aside-header">Zero percent commission</h1>
+								<p className='text-wrap signup-aside-text'>
+									All deliveries through the Seconds API and dashboard are all without commission
+									fees.
+								</p>
+							</div>
+							<div className='mt-4'>
+								<h1 className="signup-aside-header">24/7 reliable customer support</h1>
+								<p className='text-wrap signup-aside-text'>
+									Our team of specialists are always available to help via email, zoom, or call.
+								</p>
+							</div>
+							<div className='mt-4'>
+								<p className="text-wrap signup-aside-text">
+									Thank you for choosing us. We look forward to being a reliable partner to your
+									business.
+								</p>
+							</div>
+							<div className='mt-4'>
+								<span className="signup-aside-text">Ola and Chisom</span>
+								<br />
+								<span className="signup-aside-text">Founders, Seconds</span>
+							</div>
 						</div>
-					)}
-					<Formik
-						validationSchema={SignUpSchema}
-						initialValues={{
-							firstname: '',
-							lastname: '',
-							email: '',
-							company: '',
-							password: '',
-							phone: '',
-							address: '',
-							profileImage: {
-								file: '',
-								data: '',
-							},
-							apiKey: '',
-							stripeCustomerId: '',
-						}}
-						onSubmit={(values, actions) => {
-							console.log(values);
-							// const formData = this.mapStateToFormData();
-							dispatch(authUser('register', values))
-								.then(() => props.history.push('/home'))
-								.catch(err => console.log(err));
-						}}
-					>
-						{({
-							values,
-							errors,
-							touched,
-							handleChange,
-							handleBlur,
-							handleSubmit,
-							isSubmitting,
-							setFieldValue,
-							/* and other goodies */
-						}) => (
-							<form onSubmit={handleSubmit} className='signupForm'>
-								<div className='row'>
-									<div className='col-md-6 col-lg-6 pb-xs-4'>
-										<input
-											autoComplete='given-name'
-											type='text'
-											name='firstname'
-											placeholder='First Name'
-											className='form-control rounded-3'
-											onBlur={handleBlur}
-											onChange={handleChange}
-										/>
-										<ErrorField name="firstname" />
-									</div>
-									<div className='col-md-6 col-lg-6 mt-md-0 mt-sm-3'>
-										<input
-											autoComplete='family-name'
-											type='text'
-											name='lastname'
-											placeholder='Last Name'
-											className='form-control rounded-3'
-											onBlur={handleBlur}
-											onChange={handleChange}
-										/>
-										<ErrorField name="lastname" />
-									</div>
-								</div>
-								<div className='row mt-3'>
-									<div className='col-md-6 col-lg-6 pb-xs-4'>
-										<input
-											autoComplete='organization'
-											type='text'
-											name='company'
-											placeholder='Company Name'
-											className='form-control rounded-3'
-											onBlur={handleBlur}
-											onChange={handleChange}
-										/>
-										<ErrorField name="company" />
-									</div>
-									<div className='col-md-6 col-lg-6 pb-xs-4 mt-md-0 mt-sm-3'>
-										<input
-											autoComplete='tel'
-											type='tel'
-											name='phone'
-											placeholder='Phone Number'
-											className='form-control rounded-3'
-											onBlur={handleBlur}
-											onChange={handleChange}
-										/>
-										<ErrorField name="phone" />
-									</div>
-								</div>
-								<div className='mt-sm-3 mt-md-4'>
-									<GooglePlaceAutocomplete
-										autocompletionRequest={{
-											componentRestrictions: {
-												country: ['GB'],
-											},
-										}}
-										apiOptions={{
-											language: 'GB',
-											region: 'GB',
-										}}
-										selectProps={{
-											onChange: ({ label }) => {
-												setFieldValue('address', label);
-												console.log(label, values);
-											},
-											placeholder: "Address"
-										}}
-										apiKey={process.env.REACT_APP_GOOGLE_PLACES_API_KEY}
-									/>
-								</div>
-								<div className='mt-3'>
-									<input
-										autoComplete='email'
-										type='email'
-										name='email'
-										placeholder='Business Email'
-										className='form-control rounded-3'
-										onBlur={handleBlur}
-										onChange={handleChange}
-									/>
-									<ErrorField name="email" />
-								</div>
-								<div className='mt-3'>
-									<input
-										autoComplete='new-password”'
-										type='password'
-										name='password'
-										placeholder='Password'
-										className='form-control rounded-3'
-										onBlur={handleBlur}
-										onChange={handleChange}
-									/>
-									<ErrorField name="password" />
-								</div>
-								<div>
-									<button type='submit' className='signUp w-100'>
-										Sign Up
-									</button>
-								</div>
-							</form>
+					</div>
+				</div>
+				<div className='col-sm-12 col-md-9 w-sm my-auto mx-auto py-sm-5 px-md-5 px-sm-3'>
+					<div className='d-flex flex-grow-1 justify-content-center flex-column'>
+						<div className='py-4'>
+							<h2 className='signup-header pb-2'>Sign up for your account</h2>
+							<span>Lets get you all set up so you can get started</span>
+						</div>
+						{errors.message && (
+							<div className='alert alert-danger alert-dismissible' role='alert'>
+								<span>{errors.message}</span>
+								<button
+									onClick={() => dispatch(removeError())}
+									type='button'
+									className='btn btn-close'
+								/>
+							</div>
 						)}
-					</Formik>
-					<div className='text-center pt-4'>
-						<span className='text-center text-muted text-light'>
-							Already have an account?&nbsp;<Link to='/login'>Login here!</Link>
-						</span>
+						<Formik
+							validationSchema={SignUpSchema}
+							validateOnChange={false}
+							validateOnBlur={false}
+							initialValues={{
+								firstname: '',
+								lastname: '',
+								email: '',
+								company: '',
+								password: '',
+								phone: '',
+								address: '',
+								profileImage: {
+									file: '',
+									data: '',
+								},
+								apiKey: '',
+								stripeCustomerId: '',
+							}}
+							onSubmit={(values, actions) => {
+								console.log(values);
+								// const formData = this.mapStateToFormData();
+								dispatch(authUser('register', values))
+									.then(() => props.history.push('/home'))
+									.catch(err => console.log(err));
+							}}
+						>
+							{({
+								values,
+								errors,
+								touched,
+								handleChange,
+								handleBlur,
+								handleSubmit,
+								isSubmitting,
+								setFieldValue,
+								/* and other goodies */
+							}) => (
+								<form onSubmit={handleSubmit} className='signupForm'>
+									<div className='row'>
+										<div className='col-md-6 col-lg-6 pb-xs-4'>
+											<label className="mb-2" htmlFor='firstname'>First Name</label>
+											<input
+												autoComplete='given-name'
+												type='text'
+												id='firstname'
+												name='firstname'
+												className='form-control rounded-3'
+												onBlur={handleBlur}
+												onChange={handleChange}
+											/>
+											<ErrorField name='firstname' classNames="text-end" />
+										</div>
+										<div className='col-md-6 col-lg-6 mt-md-0 mt-sm-3'>
+											<label className="mb-2" htmlFor='lastname'>Last Name</label>
+											<input
+												autoComplete='family-name'
+												type='text'
+												id='lastname'
+												name='lastname'
+												className='form-control rounded-3'
+												onBlur={handleBlur}
+												onChange={handleChange}
+											/>
+											<ErrorField name='lastname' classNames="text-end" />
+										</div>
+									</div>
+									<div className='row mt-3'>
+										<div className='col-md-6 col-lg-6 pb-xs-4'>
+											<label className="mb-2" htmlFor='company'>Company</label>
+											<input
+												autoComplete='organization'
+												type='text'
+												id='company'
+												name='company'
+												className='form-control rounded-3'
+												onBlur={handleBlur}
+												onChange={handleChange}
+											/>
+											<ErrorField name='company' classNames="text-end" />
+										</div>
+										<div className='col-md-6 col-lg-6 pb-xs-4 mt-md-0 mt-sm-3'>
+											<label className="mb-2" htmlFor='company'>Phone</label>
+											<input
+												autoComplete='tel'
+												type='tel'
+												id='phone'
+												name='phone'
+												className='form-control rounded-3'
+												onBlur={handleBlur}
+												onChange={handleChange}
+											/>
+											<ErrorField name='phone' classNames="text-end" />
+										</div>
+									</div>
+									<div className='mt-3'>
+										<label className="mb-2" htmlFor='company-address'>Company address</label>
+										<GooglePlaceAutocomplete
+											autocompletionRequest={{
+												componentRestrictions: {
+													country: ['GB'],
+												},
+												types: ['geocode', 'establishment']
+											}}
+											apiOptions={{
+												language: 'GB',
+												region: 'GB',
+											}}
+											selectProps={{
+												onChange: ({ label }) => {
+													setFieldValue('address', label);
+													console.log(label, values);
+												},
+											}}
+											apiKey={process.env.REACT_APP_GOOGLE_PLACES_API_KEY}
+										/>
+										<ErrorField name='address' classNames="text-end" />
+									</div>
+									<div className='mt-3'>
+										<label className="mb-2" htmlFor='email'>Email address</label>
+										<input
+											autoComplete='email'
+											type='email'
+											id='email'
+											name='email'
+											className='form-control rounded-3'
+											onBlur={handleBlur}
+											onChange={handleChange}
+										/>
+										<ErrorField name='email' />
+									</div>
+									<div className='mt-3'>
+										<label className="mb-2" htmlFor='password'>Password</label>
+										<input
+											autoComplete='new-password”'
+											type='password'
+											name='password'
+											className='form-control rounded-3'
+											onBlur={handleBlur}
+											onChange={handleChange}
+										/>
+										<i>{eye}</i>
+										<ErrorField name='password' />
+									</div>
+									<div>
+										<button type='submit' className='btn btn-dark btn-lg w-100 mt-4'>
+											Sign Up
+										</button>
+									</div>
+								</form>
+							)}
+						</Formik>
+						<div className='pt-4'>
+							<span className='text-primary fw-bold'>
+								Already have an account?&nbsp;<Link to='/login'>Login here!</Link>
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
-			<h6 className='motto'>Automated one hour and same day deliveries for your customers</h6>
 		</div>
 	);
 }
--- src/validation/index.js
+++ src/validation/index.js
@@ -4,13 +4,13 @@ const phoneRegExp2 =
 	/^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$/;
 
 export const SignUpSchema = Yup.object().shape({
-	firstname: Yup.string().max(20, 'Max. 20 characters').required('Required'),
-	lastname: Yup.string().max(20, 'Max. 20 characters').required('Required'),
-	company: Yup.string().max(25, 'Max. 25 characters').required('Required'),
-	email: Yup.string().email('Invalid email address!').required('Required'),
-	phone: Yup.string().matches(phoneRegExp2, 'Phone number is not valid').required('Required'),
-	address: Yup.string().required('Required'),
-	password: Yup.string().min(8, 'Min. 8 characters').required('Required'),
+	firstname: Yup.string().max(20, 'Max. 20 characters').required('*Required'),
+	lastname: Yup.string().max(20, 'Max. 20 characters').required('*Required'),
+	company: Yup.string().max(25, 'Max. 25 characters').required('*Required'),
+	email: Yup.string().email('Invalid email address!').required('*Required'),
+	phone: Yup.string().matches(phoneRegExp2, 'Phone number is not valid').required('*Required'),
+	address: Yup.string().required('*Required'),
+	password: Yup.string().min(8, 'Min. 8 characters').required('*Required'),
 });
 
 export const CreateOrderSchema = Yup.object().shape({
