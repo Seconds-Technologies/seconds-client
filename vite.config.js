2d6f5d05440cf11991960fead5162a773ac950ec
Chipzstar <chipzstar.dev@googlemail.com>
2022-06-06 18:01:42 +0100

    add api + server proxies to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 
 export default ({ mode }) => {
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
 	return defineConfig({
 		plugins: [react()],
 		define: {
@@ -10,14 +12,14 @@ export default ({ mode }) => {
 		server: {
 			proxy: {
 				'/server': {
-					target: "http://localhost:8081",
+					target: process.env.VITE_SERVER_HOST,
 					changeOrigin: true,
-					secure: false
+					secure: process.env.VITE_ENV !== "local"
 				},
 				'/api': {
-					target: "http://localhost:3001",
+					target: process.env.VITE_API_HOST,
 					changeOrigin: true,
-					secure: false
+					secure: process.env.VITE_ENV !== "local"
 				}
 			}
 		},
